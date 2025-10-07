a0316569019000dd6f0d0318cc5bc4b2e5531f6d
Tutelary615 <marqueztj34@gmail.com>
2025-10-07 17:49:04 +0800

    added RecordExchangesRates.kt

---
+++ Kotlin/RecordExchangesRates.kt
@@ -0,0 +1,27 @@
+fun recordExchangesRates(foreignCurrencies: List<Currency>) {
+    var selectionInput : String = ""
+    var exchangeRateInput : String = ""
+    var selectedCurrencyIdx : Int = -1
+    var newExchangeRate : Double = 0.0
+
+    fun executeRecordExchangeRate() {
+        newExchangeRate = exchangeRateInput.toDouble()
+        foreignCurrencies[selectedCurrencyIdx].setExchangeRate(newExchangeRate)
+        println("Exchange rate of ${foreignCurrencies[selectedCurrencyIdx].getCurrencyCode()} recorded")
+    }
+
+    println("Recording exchange rates")
+    do {
+        println()
+        displayAsMenu(foreignCurrencies)
+        selectionInput = getUserInput("Select foreign currency: ")
+        if (isValidMenuInput(selectionInput, foreignCurrencies.size)) {
+            selectedCurrencyIdx = selectionInput.toInt() - 1
+            exchangeRateInput = getUserInput("Exchange rate")
+            if (isValidAmount(exchangeRateInput)) {
+                executeRecordExchangeRate()
+            }
+        }
+    } while (willRepeatTransaction("Would you like to record another exchange rate?"))
+    println()
+}
