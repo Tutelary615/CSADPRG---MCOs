8e11fe4ba97f45e5a34bc15acc92dabb9b579ad4
Tutelary615 <marqueztj34@gmail.com>
2025-10-18 23:35:50 +0800

    Added documentations and removed unused methods

--- Kotlin/Account.kt
+++ Kotlin/Account.kt
@@ -1,5 +1,12 @@
+/**
+ * class that represents an account
+ *
+ * @constructor creates an account with name set to a placeholder string "<No name registered>",
+ * balance of 0.00, an interest rate of 0.05 and a set currency
+ * @param accountCurrency
+ */
 class Account(accountCurrency: Currency) {
-    private var name = "(No name registered)"
+    private var name : String = "<No name registered>"
     private var balance : Double = 0.0
     private val interestRate : Double = 0.05
     private val accountCurrency : Currency
@@ -8,54 +15,54 @@ class Account(accountCurrency: Currency) {
         this.accountCurrency = accountCurrency
     }
 
-    fun deposit(amount: Double) : Boolean {
-        if (amount > 0.0) {
-            balance += amount
-            return true
-        } else {
-            return false
-        }
+    /**
+     * deposits an amount to the account
+     * @param amount
+     */
+    fun deposit(amount: Double) {
+        balance += amount
     }
 
-    fun deposit(sourceCurrency: Currency, amount: Double) {
-        if (amount > 0.0) {
-            balance += amount * sourceCurrency.getExchangeRate();
-        }
+    /**
+     * withdraws an amount from the account
+     * @param amount
+     */
+    fun withdraw(amount: Double) {
+       balance -= amount;
     }
 
-   fun withdraw(amount: Double) {
-       if (balance >= amount && amount > 0.0) {
-           balance -= amount
-       }
-   }
-
-   fun withdraw(sourceCurrency: Currency, amount: Double) : Boolean {
-       val amountToWithdraw = amount * sourceCurrency.getExchangeRate();
-
-       if (balance >= amountToWithdraw) {
-           balance -= amountToWithdraw
-           return true
-       } else {
-           return false
-       }
-   }
-
-   fun getName() : String {
+    /**
+     * @return the name of the account holder
+     */
+    fun getName() : String {
        return name
-   }
+    }
 
+    /**
+     * @return the balance of the account
+     */
     fun getBalance() : Double {
         return balance
     }
 
+    /**
+     * @return the interest rate of the account
+     */
     fun getInterestRate() : Double {
         return interestRate
     }
 
+    /**
+     * @return the currency of the account
+     */
     fun getAccountCurrency() : Currency {
         return accountCurrency
     }
 
+    /**
+     * sets the name of the account holder
+     * @param name name that account name will be set to
+     */
     fun setName(name: String) {
         this.name = name
     }
--- Kotlin/Currency.kt
+++ Kotlin/Currency.kt
@@ -1,3 +1,10 @@
+/**
+ * class that represents a currency
+ *
+ * @constructor creates a currency a set name, currency code, and an exchange rate of -1
+ * @param name
+ * @param currencyCode
+ */
 class Currency(name : String, currencyCode : String)  {
 
     private val name : String
@@ -9,18 +16,31 @@ class Currency(name : String, currencyCode : String)  {
         this.currencyCode = currencyCode
     }
 
+    /**
+     * @return name of the currency
+     */
     fun getName() : String {
         return name
     }
 
+    /**
+     * sets the exchange rate of the currency
+     * @param exchangeRate
+     */
     fun setExchangeRate(exchangeRate : Double) {
         this.exchangeRate = exchangeRate
     }
 
+    /**
+     * @return the currency code
+     */
     fun getCurrencyCode() : String {
         return currencyCode
     }
 
+    /**
+     * @return the exchange rate of the currency
+     */
     fun getExchangeRate() : Double {
         return exchangeRate
     }
