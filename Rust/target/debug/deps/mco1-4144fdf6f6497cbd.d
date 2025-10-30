25250c857b7632b3ad1bdb8ea4fe6d3d3a384c80
John Thomas Marquez <marqueztj34@gmail.com>
2025-10-30 20:27:33 +0800

    Delete Rust directory

--- Rust/target/debug/deps/mco1-4144fdf6f6497cbd.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Nathan\Desktop\Education\DLSU\TRIMESTER 4\CSADPRG\Rust Program\csadprg-rust\mco1\target\debug\deps\mco1-4144fdf6f6497cbd.d: src\main.rs
-
-C:\Users\Nathan\Desktop\Education\DLSU\TRIMESTER 4\CSADPRG\Rust Program\csadprg-rust\mco1\target\debug\deps\libmco1-4144fdf6f6497cbd.rmeta: src\main.rs
-
-src\main.rs:
--- Rust/target/debug/deps/mco1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Nathan\Desktop\Education\DLSU\TRIMESTER 4\CSADPRG\Rust Program\csadprg-rust\mco1\target\debug\deps\mco1.d: src\main.rs
-
-C:\Users\Nathan\Desktop\Education\DLSU\TRIMESTER 4\CSADPRG\Rust Program\csadprg-rust\mco1\target\debug\deps\mco1.exe: src\main.rs
-
-src\main.rs:
