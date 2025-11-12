7e530e91aab23b3e5423ae65aa45b91a6803fa0d
nathan <nathangabrieldolot@gmail.com>
2025-11-12 21:31:42 +0800

    fix: report2 capitalization fix

--- Rust/target/debug/deps/hello_world.d
+++ Rust/target/debug/deps/hello_world.d
@@ -1,5 +1,5 @@
-C:\Users\Nathan\Desktop\Education\DLSU\TRIMESTER 4\CSADPRG\Rust Program\csadprg-rust\mco2\target\debug\deps\hello_world.d: src\main.rs
+C:\Users\Nathan\Desktop\Education\DLSU\TRIMESTER 4\CSADPRG\Rust Program\csadprg-rust\mco2_submission\CSADPRG---MCOs\Rust\target\debug\deps\hello_world.d: src\main.rs
 
-C:\Users\Nathan\Desktop\Education\DLSU\TRIMESTER 4\CSADPRG\Rust Program\csadprg-rust\mco2\target\debug\deps\hello_world.exe: src\main.rs
+C:\Users\Nathan\Desktop\Education\DLSU\TRIMESTER 4\CSADPRG\Rust Program\csadprg-rust\mco2_submission\CSADPRG---MCOs\Rust\target\debug\deps\hello_world.exe: src\main.rs
 
 src\main.rs:
