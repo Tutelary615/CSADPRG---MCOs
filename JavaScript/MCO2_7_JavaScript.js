c4e27608518138df110a2dbada516df85bf53566
janaquino8 <janaquino53@gmail.com>
2025-11-12 16:18:20 +0800

    feat: add invalid data export

--- JavaScript/MCO2_7_JavaScript.js
+++ JavaScript/MCO2_7_JavaScript.js
@@ -173,7 +173,7 @@ async function loadFile() {
     process.stdout.write(`${formatWholeNumber(data.length)} filtered for 2021-2023), ${formatWholeNumber(invalid.length)} invalid rows filtered out.`);
 
     // Exporting error rows to a separate CSV file
-    console.log("\n\nExporting invalid rows...");
+    console.log("\n\nExporting invalid data...");
     await writeCsvFile(invalid, 'invalid_data.csv');
     return data;
 }
